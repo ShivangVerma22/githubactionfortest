a3a18490b13b5a1c926343b83748be73a2d5efff
Shivang Verma <shivangverma7355@gmail.com>
2026-07-09 12:07:39 +0530

    this is my testing actions.

---
+++ tests/testcases.py
@@ -0,0 +1,26 @@
+from src.Calculate import add,sub,mul,div
+
+
+#now making function for testing and usinf assert keyword for output varify
+def test_add():
+    assert add(5,6)==11
+    assert add(-1,-1)==-2
+    assert add(7,9)==16
+
+def test_sub():
+    assert sub(5,6)==-1
+    assert sub(-1,-1)==0
+    assert sub(9,7)==2
+
+def test_mul():
+    assert mul(5,6)==30
+    assert mul(-1,-1)==1
+    assert mul(-7,9)==-63
+
+def test_div():
+    assert div(6,6)==1
+    assert add(-1,1)==-1
+    assert add(14,7)==2
+    
+    
+    
